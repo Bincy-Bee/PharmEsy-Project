62b4ae6273b4dbd62b2fdd00e883085f9ea1866e
Bhavin Patel <patelbhavin7784@gmail.com>
2023-09-01 09:50:20 +0530

    Header Responsive

--- javascript/medicine.js
+++ javascript/medicine.js
@@ -1,5 +1,7 @@
-import {nav} from "../components/nav.js"
-document.getElementById("navbar").innerHTML= nav();
+import { nav,footer } from "../components/nav.js";
+
+document.getElementById("navbar").innerHTML = nav();
+document.getElementById("footer").innerHTML = footer();
 
 let fil = [];
 let id = 1;
@@ -37,7 +39,7 @@ let dispalymed =(data)=>{
                 .then((data)=>{
                     console.log(data)
                     localStorage.setItem("productDetails", JSON.stringify(data));
-                    window.location.href="/PharmEsy-Project/pages/product.html";
+                    window.location.href="/pages/product.html";
                 })
           
         })
